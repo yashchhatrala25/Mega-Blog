976c3fe2f836e6cce617ade26f3b9ac2298cd065
yashchhatrala25 <yashchhatrala25@gmail.com>
2024-02-29 00:33:00 +0530

    Create Blog using React Redux-Toolkit

---
+++ src/components/Select.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useId } from 'react'
+
+const Select = ({
+    options,
+    label,
+    className,
+    ...props
+}, ref) => {
+    const id = useId();
+    return (
+        <div className='w-full'>
+            {label && <label htmlFor={id} className=''></label>}
+            <select {...props} id={id} ref={ref} className={`px-3 py-2 rounded-lg outline-none text-black bg-white focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}>
+                {options?.map((option) => (
+                    <option key={option} value={option}>{option}</option>
+                ))}
+            </select>
+        </div>
+    )
+}
+
+export default React.forwardRef(Select)
---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer"; 
+import Container from "./container/Container"
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    SignUp,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Button,
+    Input,
+    Select
+}
